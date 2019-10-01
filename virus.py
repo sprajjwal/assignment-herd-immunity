49c389462ebd02720b301c6382b8833577419d38
= <zainr7989@gmail.com>
2019-09-30 18:26:04 -0700

    Add test instance of Virus.

--- virus.py
+++ virus.py
@@ -8,9 +8,18 @@ class Virus(object):
 
 
 def test_virus_instantiation():
-    #TODO: Create your own test that models the virus you are working with
+    # TODO: Create your own test that models the virus you are working with
     '''Check to make sure that the virus instantiator is working.'''
     virus = Virus("HIV", 0.8, 0.3)
+
+    # another test Virus instance
+    another_virus = Virus("Smallpox", 0.95, 0.25)
+
+    # assert statements
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
+    assert another_virus.name == "Smallpox"
+    assert another_virus.repro_rate == 0.95
+    assert another_virus.mortality_rate == 0.25
