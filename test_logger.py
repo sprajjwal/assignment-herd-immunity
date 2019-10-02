bf95d9ae5cb367aab6b68f33ddfe9319e3fdea90
= <zainr7989@gmail.com>
2019-10-02 15:14:45 -0700

    Complete test for constructor.

--- test_logger.py
+++ test_logger.py
@@ -3,17 +3,20 @@ import logger
 
 
 class TestLogger(unittest.TestCase):
-    def test__init__():
+    def test__init__(self):
         """Check to make sure Logger object instantiation works properly."""
+        log_file = logger.Logger("my_file.txt")
 
-    def test_write_metadata():
+        assert log_file.file_name == "my_file.txt"
+
+    def test_write_metadata(self):
         pass
 
-    def test_log_interaction():
+    def test_log_interaction(self):
         pass
 
-    def test_log_infection_survival():
+    def test_log_infection_survival(self):
         pass
 
-    def test_log_time_step():
+    def test_log_time_step(self):
         pass
