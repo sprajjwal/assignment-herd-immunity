9ec17d3df2d11c63a0016d327ec403e7eec676d2
= <zainr7989@gmail.com>
2019-10-02 15:02:05 -0700

    Add test suite file for logger.py.

---
+++ test_logger.py
@@ -0,0 +1,19 @@
+import unittest
+import logger
+
+
+class TestLogger(unittest.TestCase):
+    def test__init__():
+        """Check to make sure Logger object instantiation works properly."""
+
+    def test_write_metadata():
+        pass
+
+    def test_log_interaction():
+        pass
+
+    def test_log_infection_survival():
+        pass
+
+    def test_log_time_step():
+        pass
