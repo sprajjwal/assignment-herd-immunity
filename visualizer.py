3c18b4d308d05cfa585fff77a8b245e8fea98d6f
= <zainr7989@gmail.com>
2019-10-08 10:57:21 -0700

    [ADD] Bar graph method and test plot.

--- visualizer.py
+++ visualizer.py
@@ -1,33 +1,76 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from pylab import *
+plt.rcdefaults()
 
 '''
-Info for using matplotlib and numpy came from this Medium post:
+Info for using matplotlib and numpy came from these tutorials:
 https://towardsdatascience.com/5-quick-and-easy-data-visualizations-in-python-
 with-code-a2284bae952f
+
+https://pythonspot.com/matplotlib-bar-chart/
 '''
 
 
 class Visualizer:
-    pass
+    def __init__(self):
+        pass
 
+    def bar_graph(self, y_label, x_label, title):
+        """Plot a bar histogram showing numbers of alive people who are
+           either vaccinated, infected, or neither during one time step.
+        """
+        populations = ["Vaccinated", "Infected", "Neither"]
+        y_pos = np.arange(len(populations))
+        num_alive = [10, 40, 70]
+        plt.bar(y_pos, num_alive, align='center', alpha=0.5)
+        plt.xticks(y_pos, populations)
+        plt.ylabel(y_label)
+        plt.xlabel(x_label)
+        plt.title(title)
 
-def lineplot(x_data, y_data, x_label="", y_label="", title=""):
-    # Create the plot object
-    _, ax = plt.subplots()
+        plt.show()
 
-    # Plot the best fit line, set the linewidth (lw), color and
-    # transparency (alpha) of the line
-    ax.plot(x_data, y_data, lw=2, color='#539caf', alpha=1)
+    """
+        # def histogram(self, data, n_bins, x_label,
+        # y_label, title, cumulative):
+        # _, ax = plt.subplots()
+        population_sizes = [20, 30, 70]
+        bins = [2, 3]
+        plt.hist(population_sizes, bins, histtype="bar", rwidth=0.8)
+        plt.set_ylabel(y_label)
+        plt.set_xlabel(x_label)
+        plt.set_title(title)
+        plt.show()
+        # ax.hist(data, n_bins, cumulative, color='#539caf')
+        # ax.set_ylabel(y_label)
+        # ax.set_xlabel(x_label)
+        # ax.set_title(title)
 
-    # Label the axes and provide a title
-    ax.set_title(title)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
+        # ax.show()
 
-    plt.show()
+    def lineplot(self, x_data, y_data, x_label="", y_label="", title=""):
+        # Create the plot object
+        _, ax = plt.subplots()
+
+        # Plot the best fit line, set the linewidth (lw), color and
+        # transparency (alpha) of the line
+        ax.plot(x_data, y_data, lw=2, color='#539caf', alpha=1)
+
+        # Label the axes and provide a title
+        ax.set_title(title)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+
+        plt.show()
+    """
 
 
 if __name__ == "__main__":
-    lineplot(5, 6, "Number of Days", "Number of Survivors",
-             "Herd Immunity Simulation")
+    graph = Visualizer()
+    graph.bar_graph("Number of Survivors", "Populations in Simulation",
+                    "Herd Immunity Defense Against Disease Spread")
+    # 5, 2, "Populations", "Number of Survivors",
+    # "Herd Immunity Defense Against Disease Spread", False)
+    # graph.lineplot(arange(5), arange(5), "Number of Days",
+    #                 "Number of Survivors", "Herd Immunity Simulation")
