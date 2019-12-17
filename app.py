46d2d69d142b00a56a67e6017b450b2c88a98c42
= <zainr7989@gmail.com>
2019-12-17 16:23:28 -0500

    [ADD] Base for app routes.

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ graph = visualizer.Visualizer("Number of Survivors",
                                "Spread"))
 
 
+@app.route("/", methods=['GET', 'POST'])
+def simulation_params():
+    '''User sees a form to input the parameters of the simulation.'''
+    pass
+
+
+@app.route("/simulation", methods=['GET'])
+def show_results():
+    '''Show the steps of the simulation.'''
+    pass
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
