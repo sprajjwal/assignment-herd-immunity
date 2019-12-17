adb4269da2014383fba06543e1bfe348de9e56cf
= <zainr7989@gmail.com>
2019-12-17 16:18:16 -0500

    [ADD] app and visualizer objects.

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 from visualizer import Visualizer
 from simulation import Simulation
+
+
+app = Flask(__name__)
+graph = visualizer.Visualizer("Number of Survivors",
+                              ("Herd Immunity Defense Against Disease " +
+                               "Spread"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
