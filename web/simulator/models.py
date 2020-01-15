c4839dfd187b6a51fc9867f1c8d104cb9cfd4a58
= <zainr7989@gmail.com>
2020-01-14 19:13:21 -0800

    [Add] Blank models, fix imports.

--- web/simulator/models.py
+++ web/simulator/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import analysis.person as ap
+import analysis.simulation as a_s
+import analysis.virus as av
+import analysis.visualizer as avl
 
-# Create your models here.
+
+class ImmunityTest(models.Model):
+    '''An experiment by the user to test the herd immunity of a population.'''
+    pass
+
+
+class Graph(models.Model):
+    '''A visual representation of a time step for a Simulation.'''
+    pass
