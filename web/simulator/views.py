f8ccb7e6f2b2e5d113b72c29a1cf2941d12183ba
= <zainr7989@gmail.com>
2020-01-15 14:08:42 -0800

    [Add] ExperimentCreate view declaration.

--- web/simulator/views.py
+++ web/simulator/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (FormView, CreateView, ModelFormMixin,
+                                       UpdateView)
+from simulator.models import Experiment, TimeStep
+from simulator.forms import ExperimentForm
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+
+class ExperimentCreate(CreateView):
+    '''User is able to make a new experiment on the system.'''
+    pass
