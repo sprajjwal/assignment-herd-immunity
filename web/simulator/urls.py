0709437769b08e7436a1b9d626f3a75e0eb49153
= <zainr7989@gmail.com>
2020-01-16 10:30:04 -0800

    [Add] ExperimentDetail view controller.

--- web/simulator/urls.py
+++ web/simulator/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
     ExperimentCreate,
+    ExperimentDetail
 )
 
 app_name = 'simulator'
 urlpatterns = [
     path('', ExperimentCreate.as_view(), name='simulation_creator'),
+    path('<int:pk>/', ExperimentDetail.as_view(), name='experiment_detail'),
 ]
--- web/simulator/views.py
+++ web/simulator/views.py
@@ -21,3 +21,28 @@ class ExperimentCreate(CreateView):
         self.object = form.save()
         self.object.run_experiment()
         return HttpResponseRedirect(self.get_success_url())
+
+
+class ExperimentDetail(DetailView):
+    '''Displays a page with results of a specific experiment.'''
+    model = Experiment
+    template_name = 'simulator/results.html'
+
+    def get(self, request, pk):
+        """Renders a page to show the boarding instructions for a single Trip.
+
+           Parameters:
+           request(HttpRequest): the GET request sent to the server
+           pk(int): unique id value of the Experiment instance
+
+           Returns:
+           HttpResponse: the view of the detail template
+
+        """
+        experiment = self.get_queryset().get(pk=pk)
+        time_steps = TimeStep.objects.filter(experiment=experiment)
+        context = {
+            'experiment': experiment,
+            'time_steps': time_steps
+        }
+        return render(request, self.template_name, context)
