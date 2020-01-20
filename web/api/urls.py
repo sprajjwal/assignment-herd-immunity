35a8576ccf8ccc3b3808d03b578592e6bd9afaf6
= <zainr7989@gmail.com>
2020-01-19 19:34:45 -0800

    [Add] Beginning of AJAX integration.

--- web/api/urls.py
+++ web/api/urls.py
@@ -4,5 +4,5 @@ from .views import ListTimeStepData
 app_name = 'api'
 urlpatterns = [
     # used to send data to use for AJAX calls, then make graphs with Chart.js
-    path('<int:pk>/chart/data', ListTimeStepData.as_view()),
+    path('<int:pk>/chart/data', ListTimeStepData.as_view(), name="data"),
 ]
