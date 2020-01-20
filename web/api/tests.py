3064c6bf28b4824e2090544833351fbff1d5bdd7
= <zainr7989@gmail.com>
2020-01-20 13:30:51 -0800

    [Add] TestCases for simulator and api.

--- web/api/tests.py
+++ web/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from rest_framework.test import (
+    APIRequestFactory, APITestCase
+)
+from rest_framework import status
+from django.urls import reverse
+from wiki.models import Experiment, TimeStep
+from api.serializers import ExperimentSerializer, TimeStepSerializer
+from api.views import TimeStepData
 
-# Create your tests here.
+
+class TimeStepDataTests(TestCase):
+    """
+    This endpoint returns data about the final TimeStep related to an
+    Experiment.
+    """
+    pass
--- web/simulator/tests.py
+++ web/simulator/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
+from .models import Experiment, TimeStep
+from django.urls import reverse
+from .views import (
+    ExperimentCreate,
+    ExperimentDetail,
+    ExperimentList,
+    show_landing,
+    show_about_page,
+)
 
-# Create your tests here.
+
+class ExperimentCreateTests(TestCase):
+    '''A user experiments with simulation conditions on the site.'''
+    pass
+
+
+class ExperimentListTests(TestCase):
+    '''A user sees the experiments ran by other users.'''
+    pass
+
+
+class ExperimentDetailTests(TestCase):
+    '''A user sees in-depth information about the epidemic they simulated.'''
+    pass
+
+
+class LandingAndAboutPageTests(TestCase):
+    '''Users view information about why the site exists, and what it does.'''
+    pass
