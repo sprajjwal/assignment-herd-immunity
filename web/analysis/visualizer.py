04a0c3864f703697554dc3a431f503e62d8d2799
= <zainr7989@gmail.com>
2020-01-20 13:16:58 -0800

    [Fix] visualizer cleanup.

--- web/analysis/visualizer.py
+++ web/analysis/visualizer.py
@@ -23,28 +23,8 @@ class Visualizer:
         self.title = title
 
     def set_x_label(self, time_step):
-        '''Define x_label for bar graph every time step.
-        self.x_label = f'Population Sizes During Time Step {time_step}'''
-
-        """
-    def bar_graph(self, time_step, vacc, infected, dead, neither):
-        '''Plot a bar histogram showing numbers of alive people who are
-           either vaccinated, infected, or neither during one time step.
-        '''
-        populations = ["Vaccinated", "Infected", "Dead", "No Interaction"]
-        y_pos = np.arange(len(populations))
-        num_alive = [vacc, infected, dead, neither]
-        plt.bar(y_pos, num_alive, align='center', alpha=0.5)
-        plt.xticks(y_pos, populations)
-        plt.ylabel(self.y_label)
-        self.set_x_label(time_step)
-        plt.xlabel(self.x_label)
-        plt.title(self.title)
-        # show graph for 5 seconds, then close
-        plt.show(block=False)
-        plt.pause(5)
-        # plt.close()
-"""
+        '''Define x_label for bar graph every time step.'''
+        self.x_label = f'Population Sizes During Time Step {time_step}
 
 
 class WebVisualizer(Visualizer):
